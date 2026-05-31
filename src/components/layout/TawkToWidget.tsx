c8eb05479714a156f63f0f2b5194ff7ad9a6cab6
iloveyellowglove <iloveyellowglove@gmail.com>
2026-05-31 00:08:04 -0700

    fix: tawk.to widget using useEffect script injection

--- src/components/layout/TawkToWidget.tsx
+++ src/components/layout/TawkToWidget.tsx
@@ -1,30 +1,24 @@
 'use client';
 
-import Script from 'next/script';
+import { useEffect } from 'react';
 
 export default function TawkToWidget() {
-  const propertyId = process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID;
-  const widgetId = process.env.NEXT_PUBLIC_TAWKTO_WIDGET_ID;
+  useEffect(() => {
+    const propertyId = process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID;
+    const widgetId = process.env.NEXT_PUBLIC_TAWKTO_WIDGET_ID;
+    if (!propertyId || !widgetId) return;
 
-  if (!propertyId || !widgetId) return null;
+    const s1 = document.createElement('script');
+    s1.async = true;
+    s1.src = `https://embed.tawk.to/${propertyId}/${widgetId}`;
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    document.head.appendChild(s1);
 
-  return (
-    <Script
-      id="tawk-to"
-      strategy="lazyOnload"
-      dangerouslySetInnerHTML={{
-        __html: `
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/${propertyId}/${widgetId}';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-          })();
-        `,
-      }}
-    />
-  );
+    return () => {
+      document.head.removeChild(s1);
+    };
+  }, []);
+
+  return null;
 }
